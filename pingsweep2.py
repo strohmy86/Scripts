e9585c8488116035e9197889f6dacbe516f88634
Strohmy86 <strohm.luke@gmail.com>
2017-08-03 10:26:11 -0400

    Added features to pingsweep2.py

--- pingsweep2.py
+++ pingsweep2.py
@@ -6,7 +6,11 @@ import time
 from subprocess import Popen, DEVNULL
 
 # Prompt the user to input a network address
-net_addr = input("Enter a network address in CIDR format(ex.192.168.1.0/24): ")
+net_addr = input("Enter a network address in CIDR format(ex.192.168.1.0/24):  ")
+net = net_addr.replace(".", "_")
+net = net.replace("/", "-")
+# Ask user if they want the list exported to a file
+file = input("Do you want to export the list to a text file? [y/N]:  ")
 
 # Create the network
 ip_net = ipaddress.ip_network(net_addr)
@@ -23,10 +27,20 @@ while p:
     for ip, proc in p.items():
         if proc.poll() is not None: # ping finished
             del p[ip] # remove from the process list
-            if proc.returncode == 0:
+            if proc.returncode == 0 and (file == "no" or file == "n" or file == ""):
                 print('%s active' % ip)
-            #elif proc.returncode == 1:
-            #    print('%s no response' % ip)
+            elif proc.returncode == 0 and (file == "yes" or file == "y"):
+                f = open('ActiveIps-'+net+'.txt','a')
+                f.write('%s\n' % ip)
+                f.close()
             #else:
             #    print('%s error' % ip)
             break
+if file == "yes" or file == "y":
+    fr = open('ActiveIps-'+net+'.txt','r')
+    total = len(fr.readlines())
+    fr.close()
+    fw = open('ActiveIps-'+net+'.txt','a')
+    fw.write("Total Active Devices: %s" % total)
+    fw.close()
+    print("Saved list to ActiveIps-"+net+".txt")
