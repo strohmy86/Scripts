a6d4c3b2be7caf2578d706935e7c06f12b12f33e
Strohmy86 <strohm.luke@gmail.com>
2017-08-18 10:03:25 -0400

    Vastly improved certgen.py

--- certgen.py
+++ certgen.py
@@ -8,7 +8,8 @@ import paramiko
 import subprocess
 import pipes
 
-k = paramiko.RSAKey.from_private_key_file('/home/lstrohm/.ssh/Identityrsa') ## Specify private key file
+## Specify private key file
+k = paramiko.RSAKey.from_private_key_file('/home/lstrohm/.ssh/Identityrsa')
 
 ## Configure SSH connections
 fp = paramiko.SSHClient()
@@ -19,6 +20,11 @@ rad = paramiko.SSHClient()
 rad.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 rad.connect('10.14.0.26', username='root', pkey = k)
 
+## Define global variables
+path = '/media/nss/VOL1/shared/madhs01rad1/requests/'
+name = ''
+
+
 ## Function to close all SSH connections and exit the script
 def close():
     time.sleep(1)
@@ -28,46 +34,46 @@ def close():
 
 def start():
     global name
-    name = input("What is the name of the machine?  ") # First we need the machine name
     global path
-    path = path + name
+    name = input("What is the name of the machine?  ") # First we need the machine name
     ## Creates a certificate request if a PC name is given
     if name != '':
         print("Creating certificate request for " + name)
-        stdin, stdout, stderr = fp.exec_command('touch ' + path)
+        stdin, stdout, stderr = fp.exec_command('touch ' + path + name)
         time.sleep(2)
+        gen()
     elif name == '':
         print('No machine name specified.')
         time.sleep(1)
         start()
+    return
 
-path = '/media/nss/VOL1/shared/madhs01rad1/requests/'
-name = ''
 
-start()
 
 ## Checks to make sure the request was successful, then runs the cert-gen script
 def gen():
     global path
     global name
     resp = subprocess.call(
-        ['ssh', '-q', '-i', '/home/lstrohm/.ssh/Identityrsa', 'root@10.14.0.20', 'test -e ' + pipes.quote(path)])
+        ['ssh', '-q', '-i', '/home/lstrohm/.ssh/Identityrsa', 'root@10.14.0.20', 'test -e ' + pipes.quote(path + name)])
     if resp  == 0:
         print('Request created sucessfully, awating certificate generation...')
         stdin, stdout, stderr = rad.exec_command('/root/certgen/cert-gen')
         time.sleep(3)
+        certcheck()
     elif resp != 0:
         again = input('Certificate request failed! Would you like to try again? [Y/n]  ')
         if again == 'y' or again == 'yes' or again == '':
             gen()
-            certcheck()
         elif again == 'n' or again == 'no':
             print('Exiting...')
             close()
         else:
             print('Error!')
             close()
-gen()
+    return
+
+
 
 ## Checks to see if the certificate was generated correctly
 def certcheck():
@@ -80,10 +86,7 @@ def certcheck():
         again = input('Certificate generated sucessfully! Would you like to generate another certificate? [y/N]  ')
         if again == 'y' or again == 'yes':
             time.sleep(1)
-            name = ''
             start()
-            gen()
-            certcheck()
         elif again == 'n' or again == 'no' or again == '':
             print('Exiting...')
             close()
@@ -95,18 +98,17 @@ def certcheck():
         if tryAgain == 'y' or tryAgain == 'yes' or tryAgain == '':
             time.sleep(1)
             resp = subprocess.call(
-                ['ssh', '-q', '-i', '/home/lstrohm/.ssh/Identityrsa', 'root@10.14.0.20', 'test -e ' + pipes.quote(path)])
+                ['ssh', '-q', '-i', '/home/lstrohm/.ssh/Identityrsa', 'root@10.14.0.20', 'test -e ' + pipes.quote(path + name)])
             if resp == 0:
                 gen()
-                certcheck()
             elif resp != 0:
                 start()
-                gen()
-                certcheck()
         elif tryAgain == 'n' or tryAgain =='no':
             print('Exiting...')
             close()
         else:
             print('Error!')
             close()
-certcheck()
+    return
+
+start()
