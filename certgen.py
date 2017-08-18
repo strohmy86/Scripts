6c187153a47ffb65334c58379a7d94397a7673ea
Strohmy86 <strohm.luke@gmail.com>
2017-08-18 10:29:12 -0400

    Added documentation

--- certgen.py
+++ certgen.py
@@ -32,47 +32,46 @@ def close():
     rad.close()
     sys.exit()
 
+
+## Gathers PC name and initiates certificate request
 def start():
     global name
     global path
-    name = input("What is the name of the machine?  ") # First we need the machine name
-    ## Creates a certificate request if a PC name is given
-    if name != '':
+    name = input("What is the name of the machine?  ")
+    if name != '': ## Creates a certificate request if a PC name is given
         print("Creating certificate request for " + name)
         stdin, stdout, stderr = fp.exec_command('touch ' + path + name)
         time.sleep(2)
         gen()
-    elif name == '':
+    elif name == '': ## If no name is given, start the function over
         print('No machine name specified.')
         time.sleep(1)
         start()
     return
 
 
-
 ## Checks to make sure the request was successful, then runs the cert-gen script
 def gen():
     global path
     global name
-    resp = subprocess.call(
+    resp = subprocess.call( ## Checks for the request file
         ['ssh', '-q', '-i', '/home/lstrohm/.ssh/Identityrsa', 'root@10.14.0.20', 'test -e ' + pipes.quote(path + name)])
-    if resp  == 0:
+    if resp  == 0: ## If the file is present, runs the cert-gen script
         print('Request created sucessfully, awating certificate generation...')
         stdin, stdout, stderr = rad.exec_command('/root/certgen/cert-gen')
         time.sleep(3)
         certcheck()
-    elif resp != 0:
+    elif resp != 0: ## If request file is missing, prompts to try again
         again = input('Certificate request failed! Would you like to try again? [Y/n]  ')
-        if again == 'y' or again == 'yes' or again == '':
+        if again == 'y' or again == 'yes' or again == '': ## If user wants to try again, the gen() function is restarted
             gen()
-        elif again == 'n' or again == 'no':
+        elif again == 'n' or again == 'no': ## exits the script if user inputs n or no
             print('Exiting...')
             close()
-        else:
+        else: ## If anything other than an input above is given, the script errors out and exits
             print('Error!')
             close()
     return
-
 
 
 ## Checks to see if the certificate was generated correctly
@@ -80,33 +79,33 @@ def certcheck():
     global path
     global name
     cert = '/media/nss/VOL1/shared/madhs01rad1/certs/' + name + '_cert.p12'
-    gen = subprocess.call(
+    gen = subprocess.call( ## Checks to see if the certificate file was generated correctly
         ['ssh', '-q', '-i', '/home/lstrohm/.ssh/Identityrsa', 'root@10.14.0.20', 'test -e ' + pipes.quote(cert)])
-    if gen == 0:
+    if gen == 0: ## If the certificate exists, prompts user if they want to generate another
         again = input('Certificate generated sucessfully! Would you like to generate another certificate? [y/N]  ')
-        if again == 'y' or again == 'yes':
+        if again == 'y' or again == 'yes': ## If yes, the script starts from the beginning
             time.sleep(1)
             start()
-        elif again == 'n' or again == 'no' or again == '':
+        elif again == 'n' or again == 'no' or again == '': ## If no, the script exits
             print('Exiting...')
             close()
-        else:
+        else: ## If anything other than an input above is given, the script errors out and exits
             print('Error!')
             close()
-    elif gen != 0:
+    elif gen != 0: ## If certificate generation failed, promts user to try again
         tryAgain = input('Certificate generation failed. Would you like to try again? [Y/n]  ')
-        if tryAgain == 'y' or tryAgain == 'yes' or tryAgain == '':
+        if tryAgain == 'y' or tryAgain == 'yes' or tryAgain == '': ## If user inputs yes, checks for existing request
             time.sleep(1)
             resp = subprocess.call(
                 ['ssh', '-q', '-i', '/home/lstrohm/.ssh/Identityrsa', 'root@10.14.0.20', 'test -e ' + pipes.quote(path + name)])
-            if resp == 0:
+            if resp == 0: ## If request file exists, runs the gen() function
                 gen()
-            elif resp != 0:
+            elif resp != 0: ## If the request does not exist, starts the script from the beginning
                 start()
-        elif tryAgain == 'n' or tryAgain =='no':
+        elif tryAgain == 'n' or tryAgain =='no': ## Id user inputs no, exits the program
             print('Exiting...')
             close()
-        else:
+        else:## If anything other than an input above is given, the script errors out and exits
             print('Error!')
             close()
     return
