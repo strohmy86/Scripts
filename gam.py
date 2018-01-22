a68917bc8282135569268a609de59b44512664ce
Luke Strohm <strohm.luke@gmail.com>
2018-01-22 14:17:15 -0500

    Converted Gam.py to python 3.x

--- gam.py
+++ gam.py
@@ -1,95 +1,95 @@
-#!/usr/bin/python2
+#!/usr/bin/env python
 
 import os
 import time
 
-print "*********************************"
-print "*  GAM basic automation script  *"
-print "*  Frontend to  GAM by jay0lee  *"
-print "*                               *"
-print "*  Written and maintained by:   *"
-print "*        Luke Strohm            *"
-print "*    strohm.luke@gmail.com      *"
-print "*                               *"
-print "*********************************"
+print("*********************************")
+print("*  GAM basic automation script  *")
+print("*  Frontend to  GAM by jay0lee  *")
+print("*                               *")
+print("*  Written and maintained by:   *")
+print("*        Luke Strohm            *")
+print("*    strohm.luke@gmail.com      *")
+print("*                               *")
+print("*********************************")
 
 
 
 
 while True:
-    print '\n'
-    print 'Menu:'
-    print '\n'
-    print '1)\tUser Information'
-    print '2)\tExport List of All Users'
-    print '3)\tActivate Suspended User'
-    print '4)\tSuspend User'
-    print "5)\tExport a List of a User's Drive Files"
-    print "6)\tUpload a Local File To a User's Drive"
-    print "7)\tView a User's Team Drive(s)"
-    print '0)\tExit'
-    print '\n'
+    print('\n')
+    print('Menu:')
+    print('\n')
+    print('1)\tUser Information')
+    print('2)\tExport List of All Users')
+    print('3)\tActivate Suspended User')
+    print('4)\tSuspend User')
+    print("5)\tExport a List of a User's Drive Files")
+    print("6)\tUpload a Local File To a User's Drive")
+    print("7)\tView a User's Team Drive(s)")
+    print('0)\tExit')
+    print('\n')
 
-    selection=raw_input("Please Choose an Option: ")
-    if selection =='1':
-        username=raw_input("Please enter a username: ")
+    selection=input("Please Choose an Option: ")
+    if selection == '1':
+        username=input("Please enter a username: ")
         cmd="~/bin/gam/gam info user " +username
         os.system(cmd)
-        print '\n'
+        print('\n')
         time.sleep(2)
-        raw_input("Press ENTER to Continue...")
-    elif selection =='2':
+        input("Press ENTER to Continue...")
+    elif selection == '2':
         cmd="~/bin/gam/gam print users allfields > Userlist.csv"
         os.system(cmd)
-        print '\n'
-        print "Userlist.csv saved."
+        print('\n')
+        print("Userlist.csv saved.")
         time.sleep(2)
-        raw_input("Press ENTER to Continue...")
-    elif selection =='3':
-        username=raw_input("Please enter a username: ")
+        input("Press ENTER to Continue...")
+    elif selection == '3':
+        username=input("Please enter a username: ")
         cmd="~/bin/gam/gam update user " +username + " suspended off"
         cmd2="~/bin/gam/gam info user " +username
         os.system(cmd)
         time.sleep(2)
         os.system(cmd2)
-        print '\n'
+        print('\n')
         time.sleep(2)
-        raw_input("Press ENTER to Continue...")
-    elif selection =='4':
-        username=raw_input("Please enter a username: ")
+        input("Press ENTER to Continue...")
+    elif selection == '4':
+        username=input("Please enter a username: ")
         cmd="~/bin/gam/gam update user " +username + " suspended on"
         os.system(cmd)
-        print '\n'
+        print('\n')
         time.sleep(2)
-        raw_input("Press ENTER to Continue...")
-    elif selection =='5':
-        username=raw_input("Please enter a username: ")
+        input("Press ENTER to Continue...")
+    elif selection == '5':
+        username=input("Please enter a username: ")
         cmd="~/bin/gam/gam user " +username + " show filelist allfields > " +username + "-filelist.csv"
         os.system(cmd)
-        print '\n'
-        print "File list saved as " + username +"-filelist.csv."
-        print '\n'
+        print('\n')
+        print("File list saved as " + username +"-filelist.csv.")
+        print('\n')
         time.sleep(2)
-        raw_input("Press ENTER to Continue...")
-    elif selection =='6':
-        username=raw_input("Please enter a username: ")
-        filename=raw_input("Please enter the full path to the file you wish to upload: ")
+        input("Press ENTER to Continue...")
+    elif selection == '6':
+        username=input("Please enter a username: ")
+        filename=input("Please enter the full path to the file you wish to upload: ")
         cmd="~/bin/gam/gam user " +username + " add drivefile localfile " +filename
         os.system(cmd)
-        print '\n'
-        print "File uploaded successfully"
-        print '\n'
+        print('\n')
+        print("File uploaded successfully")
+        print('\n')
         time.sleep(2)
-        raw_input("Press ENTER to Continue...")
+        input("Press ENTER to Continue...")
     elif selection == '7':
-        username=raw_input("Please enter a username: ")
+        username=input("Please enter a username: ")
         cmd='~/bin/gam/gam user ' +username+ ' show teamdrives'
-        print '\n'
+        print('\n')
         os.system(cmd)
-        print '\n'
+        print('\n')
         time.sleep(2)
-        raw_input("Press ENTER to Continue...")
-    elif selection =='0':
+        input("Press ENTER to Continue...")
+    elif selection == '0':
         break
     else:
-        print "Unknown Option Selected!"
+        print("Unknown Option Selected!")
