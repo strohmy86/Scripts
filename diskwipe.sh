4c036bb921e369f67f9579ca7c1e75e27839324e
Luke Strohm <strohm.luke@gmail.com>
2018-05-03 14:29:51 -0400

    Update diskwipe.sh

--- diskwipe.sh
+++ diskwipe.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 dev="/dev/"
-disk=$(lsblk | grep disk  | awk '{print $1}' | sort | head -n 1)
+disk=$(lsblk | grep disk | awk '{print $1,$4}' | sort -rn -k2 | head -n 1 | cut -d " " -f1)
 yep=$dev$disk
+sz=$(lsblk | grep disk | awk '{print $1,$4}' | sort -rn -k2 | head -n 1 | cut -d " " -f2)
 
 echo ""
 echo "*********************************"
@@ -16,15 +17,19 @@ echo "*********************************"
 echo ""
 echo ""
 
+echo "The size of $yep is $sz."
+echo ""
 echo "$yep will be erased. THIS IS UNRECOVERABLE! resume? [Y/n] "
 echo ""
 
 read ans
 
 if [ $ans == y ]; then
-    echo "Erasing partition tatble for $yep."
+    echo "Erasing partition table for $yep."
     parted -a optimal -s $yep mklabel msdos
     echo "Done!"
+    exit 0
 else
     echo "Quitting..."
+    exit 0
 fi
