4d33da447eb8f7dcc2ae01c663934b63f39c8595
Luke Strohm <strohm.luke@gmail.com>
2018-10-25 10:36:12 -0400

    Fixed typo

--- activate-new-users.sh
+++ activate-new-users.sh
@@ -15,7 +15,7 @@ echo "*    strohm.luke@gmail.com      *"
 echo "*                               *"
 echo "*********************************"
 
-date1=`date -utc +%FT%T.%3NZ --date="7 days ago"`
+date1=`date --utc +%FT%T.%3NZ --date="7 days ago"`
 
 echo "Getting a list of suspended users..."
 
