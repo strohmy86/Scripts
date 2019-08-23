8fdc38e1ec440b0d3cf10d3ffcafaf974bafd57e
Luke Strohm <strohm.luke@gmail.com>
2019-08-23 10:53:34 -0400

    Added License to beginning of all scripts

--- pingsweep.sh
+++ pingsweep.sh
@@ -1,5 +1,27 @@
 #!/bin/bash
 
+# MIT License
+
+# Copyright (c) 2019 Luke Strohm
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
 if [ "$1" == "" ]
 then
 echo "Usage: ./pingsweep.sh [network segment]"
@@ -9,5 +31,3 @@ for x in `seq 1 254`; do
 	ping -c 1 $1.${x} | grep "64 bytes" | cut -d " " -f 4 | sed 's/.$//' &
 done
 fi
-
-
